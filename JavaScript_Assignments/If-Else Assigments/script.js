c4300d35a0ea5d199e7fea096e599e0f7fe9e1e2
CodeWithAman <amanverma78503@gmail.com>
2026-05-06 03:00:26 +0530

    feat: add HTML and JavaScript files for If-Else assignments

---
+++ JavaScript_Assignments/If-Else Assigments/script.js
@@ -0,0 +1,135 @@
+// 1.What is the purpose of if statement in JavaScript?
+// The if Statement is used to execute a block of code if a specified condition is true and to skip the block of code if the condition is false. It allows you to make decisions in your code based on certain conditions.
+
+
+
+// 2. Write a program to check if a number is greater than 10. If true print "Greater", else print "Smaller".
+// let number = prompt("Enter a number: ");
+// if(number > 10){
+//     console.log("Greater");
+// }
+// else{
+//     console.log("Smaller");
+// }
+
+
+
+
+
+// 3.Write a program to check if a number is even or odd.
+// let number = prompt("Enter a number: ");
+// if(number % 2 == 0){
+//     console.log("Even");
+// }
+// else{
+//     console.log("Odd")
+// }
+
+
+
+// 4.What is the role of else in JavaScript?
+// The else statement is used in conjunction with an if statement to specify a block of code that will be executed if the condition in the if statement evaluates to false. It provides an alternative path of execution when the condition is not met, allowing you to handle different scenarios in your code.
+
+
+
+
+// 5.Write a program to check voting eligibility (age ≥ 18 → eligible, else not eligible).
+// let age = prompt("Enter your age: ");
+// if(age >= 18){
+//     console.log("Eligible for Vote");
+// }
+// else{
+//     console.log("Not Eligible for Vote");
+// }
+
+
+
+// 6.Write a program to simulate a traffic signal (if green → go, else → stop).
+// let signal = prompt("Enter traffic signal color (green/yellow/red): ");
+// if(signal == "green"){
+//     console.log("Go Go Go.....");
+// }
+// else if(signal == "yellow"){
+//     console.log("Wait Wait Wait...");
+// }
+// else if(signal == "red"){
+//     console.log("Stop Stop...");
+// }
+// else{
+//     console.log("Invaild Signal Color");
+// }
+
+
+
+
+// 7.Write a program to check if a number is positive or not.
+// let number = prompt("Enter a number: ");
+// if(number > 0){
+//     console.log("Number is Positive");
+// }
+// else if(number == 0){
+//     console.log("Number is Zero");
+    
+// }
+// else{
+//     console.log("Number is Negative or");
+// }
+
+
+
+// 8.Write a program to check if a number is divisible by 2 using condition.
+// let number = prompt("Enter a number: ");
+// if(number % 2 == 0){
+//     console.log("Number is divisible by 2");
+// }
+// else{
+//     console.log("Number is not divisible by 2");
+// }
+
+
+
+
+// 9.What happens when the condition in if statement is false and there is an else block?
+// when the condition in an if statement is false and there is an else block, the code inside the else block will be executed.
+
+
+
+
+// 10. Write a program and explain which block will execute when condition is true and when it is false.
+// let number = prompt("Enter a number: ");
+// if(number > 10){
+//     console.log("Greater");
+// }
+// else{
+//     console.log("Smaller");
+// }
+// In this program, if the user enters a number greater than 10, the block of code inside the if statement will execute and print "Greater". If the user enters a number that is 10 or less, the condition will evaluate to false, and the block of code inside the else statement will execute, printing "Smaller".
+
+
+
+
+// 11. Create a program and trace its execution step-by-step (condition → execution).
+// let age = prompt("Enter your age: ");
+// if(age >= 18){
+//     console.log("Eligible for Vote");
+// }
+// else{
+//     console.log("Not Eligible for Vote");
+// }  
+// Step 1: The program prompts the user to enter their age and stores it in the variable 'age'.
+// Step 2: The program checks if the value of 'age' is greater than or equal to 18.
+// Step 3: If the condition is true (age is 18 or older), it executes the code inside the if block and prints "Eligible for Vote" to the console.
+// Step 4: If the condition is false (age is less than 18), it executes the code inside the else block and prints "Not Eligible for Vote" to the console.
+
+
+
+
+// 12. Write a program to check a condition using comparison operators and print the result.
+// let number1 = prompt("Enter first number: ");
+// let number2 = prompt("Enter second number: ");
+// if(number1 > number2){
+//     console.log(number1 + " is greater than " + number2);
+// }
+// else{
+//     console.log(number1 + " is not greater than " + number2);
+// } 
