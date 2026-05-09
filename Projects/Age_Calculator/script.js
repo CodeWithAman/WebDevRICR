784b5c68270ead55196b99ff434369063ca809fe
CodeWithAman <amanverma78503@gmail.com>
2026-05-10 02:50:59 +0530

    feat: update age display in HTML and enhance JavaScript for age calculation

--- Projects/Age_Calculator/script.js
+++ Projects/Age_Calculator/script.js
@@ -1,12 +1,14 @@
-document.querySelector("form").addEventListener("submit" , (event)=>{
-    event.preventDefault();
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
 
-    const DOB = document.getElementById("DOB").value;
-    const currentDOB = document.getElementById("currentDOB").value;
+  const DOB = document.getElementById("DOB").value;
+  const currentDOB = document.getElementById("currentDOB").value;
 
-    const Age = Number(currentDOB.split("-")[0]) - Number(DOB.split("-")[0])
+  const Age = Number(currentDOB.split("-")[0]) - Number(DOB.split("-")[0]);
 
-    document.getElementById("DOB").value="";
-    document.getElementById("currentDOB").value="";
-    
-})
+  document.getElementById("DOB").value = "";
+  document.getElementById("currentDOB").value = "";
+
+  document.getElementById("calculatedAge").innerHTML =
+    "Your age is " + Age + "years";
+});
