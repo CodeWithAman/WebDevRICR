7f9c36d56f850b1f8566a08049d02c8ff995b3e5
CodeWithAman <amanverma78503@gmail.com>
2026-05-15 15:26:34 +0530

    feat: enhance form validation in Coaching Institute Registration Form with error messages for full name and email

--- Projects/CoachingInstituteRegistrationForm/script.js
+++ Projects/CoachingInstituteRegistrationForm/script.js
@@ -1,23 +1,87 @@
-document.getElementById("registrationFrom").addEventListener("submit" , (event) =>{
+document
+  .getElementById("registrationFrom")
+  .addEventListener("submit", (event) => {
     event.preventDefault();
 
-    const fullName = document.getElementById("fullName").value;
-    const gender = document.querySelector("input[name='gender']:checked")?.value;
+    const fullName = document.getElementById("fullName").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const phone = document.getElementById("phone").value.trim();
+    const dob = document.getElementById("dob").value.trim();
+    const gender = document.querySelector(
+      "input[name='gender']:checked",
+    )?.value;
+    const qualification = document.getElementById("qualification").value;
+    const marks = document.getElementById("marks").value;
+    const course = document.getElementById("course").value;
+    const address = document.getElementById("address").value;
+    const city = document.getElementById("city").value;
+    const pin = document.getElementById("pin").value;
+    const timings = [];
+    document
+      .querySelectorAll("input[name='timings']:checked")
+      .forEach((item) => {
+        timing.push(item.value);
+      });
 
-    const timing = [];
+    const regisregistrationData = {
+      FullName: fullName,
+      Email: email,
+      Phone: phone,
+      DateofBirth: dob,
+      Gender: gender,
+      Qualification: qualification,
+      Marks: marks,
+      Course: course,
+      Timings: timings,
+      Address: address,
+      City: city,
+      PinCode: pin,
+    };
 
-    document.querySelectorAll("input[name='timings']:checked").forEach((item) =>{
-        timing.push(item.value)
-    })
+    validateData(regisregistrationData)
+      ? (console.log("Registration Data:", regisregistrationData),
+        alert("Registration successful! Check console for details."))
+      : alert("Registration unsuccessfull! Enter valid Data.");
+  });
 
-    console.log(gender);
-
-    console.log(timing);
-    
-})
-
-document.getElementById("registrationFrom").addEventListener("reset" , (event) =>{
-    even.preventDefault();
+document
+  .getElementById("registrationFrom")
+  .addEventListener("reset", (event) => {
+    event.preventDefault();
 
     window.location.reload();
-})
+  });
+
+function validateData(data) {
+  document.querySelectorAll(".manualError").forEach((item) => {
+    item.innerText = "";
+  });
+
+  let isVaild = true;
+
+  console.log(data);
+
+  if (!data.FullName) 
+  {
+    document.getElementById("fullNameError").innerText = "Full Name Required";
+    isVaild = false;
+  }
+  else if (!/^[A-Za-z\s]+$/.test(data.FullName)) {
+    document.getElementById("fullNameError").innerText =
+    "Only alphabets and spaces are allowed";
+    isVaild = false;
+  }
+
+
+  if(!data.Email)
+  {
+    document.getElementById("emailError").innerText = "Email Required";
+    isVaild = false;
+  }
+  else if(!/^[A-Za-z\d\_\.]+@(gmail.com|outlook.com|ricr.in)$/.test(data.Email)){
+    document.getElementById("emailError").innerText = "Only standard Email format allowed";
+    isVaild = false;
+  }
+
+  return isVaild;
+}
