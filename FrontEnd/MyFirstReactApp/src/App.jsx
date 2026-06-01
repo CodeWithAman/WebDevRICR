92b47de4fd10a87f25c9ec5cd9ef309bae37e8ad
CodeWithAman <amanverma78503@gmail.com>
2026-06-01 20:44:05 +0530

    refactor: clean up App component and remove unused CSS styles

--- FrontEnd/MyFirstReactApp/src/App.jsx
+++ FrontEnd/MyFirstReactApp/src/App.jsx
@@ -1,20 +1,29 @@
 import Header from "./components/Header";
-import Footer from "./components/Footer";
+// import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Products from "./pages/Products";
+import ContactUs from "./pages/ContactUs";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Header />
+      <BrowserRouter>
+        <Header />
 
-      <div id="abc">My First React App</div>
-      <p className="bg-danger">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores
-        provident nostrum, dolorum accusantium adipisci saepe libero deserunt
-        nam iusto quibusdam rerum blanditiis? Blanditiis quia voluptas aliquid
-        repudiandae, rerum odio ea.
-      </p>
-
-      <Footer />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/Product" element={<Products />} />
+          <Route path="/ContactUs" element={<ContactUs />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
