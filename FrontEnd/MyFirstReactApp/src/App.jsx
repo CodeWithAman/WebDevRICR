d679e4fdf7622d644e1e069895fbfe4e0f443ac0
CodeWithAman <amanverma78503@gmail.com>
2026-06-03 20:09:00 +0530

    feat: add IPL winner image to App component and update Header with icon

--- FrontEnd/MyFirstReactApp/src/App.jsx
+++ FrontEnd/MyFirstReactApp/src/App.jsx
@@ -6,6 +6,7 @@ import Products from "./pages/Products";
 import ContactUs from "./pages/ContactUs";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import iplWinner from "./assets/rcb.jpg";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -23,6 +24,8 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
+
+        <img src={iplWinner} className="w-100 h-25" />
       </BrowserRouter>
     </>
   );
--- FrontEnd/MyFirstReactApp/src/components/Header.jsx
+++ FrontEnd/MyFirstReactApp/src/components/Header.jsx
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom";
+import { FaApple } from "react-icons/fa";
 
 function Header() {
   return (
     <>
       <div className="bg-primary-subtle p-2 d-flex justify-content-between align-items-center p-3 shadow-sm">
-        <div className="text-primary fs-4 fw-bold">My Company</div>
+        <div className="text-primary fs-4 fw-bold d-flex align-items-center gap-2">
+          <FaApple className="fs-4 text-black" />
+          My Company
+        </div>
 
         <div className="d-flex gap-4">
           <Link to={"/"}>Home</Link>
