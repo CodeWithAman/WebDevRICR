b7a686f51243571092e77e3ffd6cef68455fed36
CodeWithAman <amanverma78503@gmail.com>
2026-06-06 20:17:02 +0530

    feat: implement Login and Register components with form handling and routing

--- FrontEnd/MyFirstReactApp/src/pages/Login.jsx
+++ FrontEnd/MyFirstReactApp/src/pages/Login.jsx
@@ -1,8 +1,63 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 function Login() {
+  const [userName, setUserName] = useState("");
+  const [passWord, setpassWord] = useState("");
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    
+    console.log("UserName :" , userName);
+    console.log("Password :" , passWord);
+    
+    
+  }
+
   return (
     <>
       <div className="container d-flex justify-content-center align-items-center">
-        <samp className="text-success fs-5 fw-bold">Login</samp>
+        <div className="p-3 mt-5 bg-white shadow rounded" id="login-card">
+          <h1 className="text-center">Welcome Back !!</h1>
+          <br />
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="UserName" className="fw-bold">
+              Username
+            </label>
+            <input
+              type="text"
+              name="UserName"
+              placeholder="Enter your username"
+              className="form-control"
+              value={userName}
+              onChange={(e) => {
+                setUserName(e.target.value);
+              }}
+            />
+            <br />
+            <label htmlFor="PassWord" className="fw-bold">
+              Password
+            </label>
+            <input
+              type="password"
+              name="UserName"
+              placeholder="Enter your password"
+              className="form-control"
+              value={passWord}
+              onChange={(e) => {
+                setpassWord(e.target.value);
+              }}
+            />
+            <br />
+            <div className=" text-end">
+              <button className=" btn btn-primary">Login</button>
+            </div>
+          </form>
+          <hr />
+          <div className="text-center">
+            Don't have an account ? <Link to="/register">Register</Link>
+          </div>
+        </div>
       </div>
     </>
   );
--- FrontEnd/MyFirstReactApp/src/pages/Register.jsx
+++ FrontEnd/MyFirstReactApp/src/pages/Register.jsx
@@ -1,8 +1,76 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 function Register() {
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [passWord, setpassWord] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log("UserName :", userName);
+    console.log("Email :", email);
+    console.log("Password :", passWord);
+  };
   return (
     <>
       <div className="container d-flex justify-content-center align-items-center">
-        <samp className="text-success fs-5 fw-bold">Register</samp>
+        <div className="p-3 mt-5 bg-white shadow rounded" id="login-card">
+          <h1 className="text-center">Welcome Back !!</h1>
+          <br />
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="FullName" className="fw-bold">
+              Fullname
+            </label>
+            <input
+              type="text"
+              name="FullName"
+              placeholder="Enter your fullname"
+              className="form-control"
+              value={userName}
+              onChange={(e) => {
+                setUserName(e.target.value);
+              }}
+            />
+            <br />
+            <label htmlFor="Email" className="fw-bold">
+              Email
+            </label>
+            <input
+              type="email"
+              name="Email"
+              placeholder="Enter your email"
+              className="form-control"
+              value={userName}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+            <br/>
+            <label htmlFor="PassWord" className="fw-bold">
+              Password
+            </label>
+            <input
+              type="password"
+              name="UserName"
+              placeholder="Enter your password"
+              className="form-control"
+              value={passWord}
+              onChange={(e) => {
+                setpassWord(e.target.value);
+              }}
+            />
+            <br />
+            <div className=" text-end">
+              <button className=" btn btn-primary">Login</button>
+            </div>
+          </form>
+          <hr />
+          <div className="text-center">
+            Don't have an account ? <Link to="/login">Login</Link>
+          </div>
+        </div>
       </div>
     </>
   );
