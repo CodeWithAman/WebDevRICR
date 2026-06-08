0ec5e84201e8a51f075e56fc578f077500190180
CodeWithAman <amanverma78503@gmail.com>
2026-06-08 20:13:43 +0530

    feat: enhance Home component with navigation and improved layout

--- FrontEnd/MyFirstReactApp/src/pages/Home.jsx
+++ FrontEnd/MyFirstReactApp/src/pages/Home.jsx
@@ -1,8 +1,28 @@
+import { useNavigate } from "react-router-dom";
+
 function Home() {
+
+  const navigate = useNavigate();
+
   return (
     <>
       <div className="container d-flex justify-content-center align-items-center">
-        <samp className="text-success fs-5 fw-bold">Home</samp>
+        <div className=" border p-3 w-50 mt-5 bg-light shadow rounded">
+          <h1 className="text-center">Welcome to Home Page</h1>
+          <p className="text-center">
+            we have wide range of Clothings, Footwear and Accessories for you.
+            Please check our products and enjoy shopping with us.
+          </p>
+
+          <div className=" d-flex justify-content-center mt-4">
+            <button
+              className=" btn  btn-success me-2"
+              onClick={() => navigate("/Product")}
+            >
+              Check our Product
+            </button>
+          </div>
+        </div>
       </div>
     </>
   );
