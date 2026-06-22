cf8f0db2859127930896816fcca40839442ff529
CodeWithAman <amanverma78503@gmail.com>
2026-06-22 20:26:22 +0530

    feat: add authentication and public controller functions for user login, logout, and registration

---
+++ BackEnd/FirstBackEnd/src/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+export const LoginUser = (req, res) => {
+  res.json({ message: "Login Sucessfull from Controller" });
+};
+
+export const LogoutUser = (req, res) => {
+  res.json({ message: "Logout Sucessfull from Controller" });
+};
+
+
+export const RegisterUser = (req, res) => {
+  res.json({ message: "Registration Sucessfull from Controller" });
+};
