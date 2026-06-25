b153947fe3976451a9038e5424252e4e33559788
CodeWithAman <amanverma78503@gmail.com>
2026-06-25 21:09:51 +0530

    feat: implement user registration with validation and error handling

--- BackEnd/FirstBackEnd/index.js
+++ BackEnd/FirstBackEnd/index.js
@@ -4,8 +4,11 @@ dotenv.config();
 import express from "express"; // express
 import AuthRouter from "./src/routers/auth.route.js";
 import PublicRouter from "./src/routers/public.route.js";
+import connectDB from "./src/config/dbConnection.config.js";
 
 const app = express(); // object
+
+app.use(express.json()); // middleware
 
 app.use("/auth", AuthRouter);
 app.use("/public", PublicRouter);
@@ -16,8 +19,17 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to my first backend Project" }); // api creations
 });
 
+// Default Error Handler
+app.use((err, req, res, next) => {
+  const ErrorMessage = err.message || "Internal Server Error";
+  const ErrorStatusCode = err.statusCode || 500;
+
+  res.status(ErrorStatusCode).json({ message: ErrorMessage });
+});
+
 const port = process.env.PORT || 5000; // verify the port
 
 app.listen(port, () => {
   console.log("Server Strated on PORT:", port); //
+  connectDB();
 });
--- BackEnd/FirstBackEnd/src/controllers/auth.controller.js
+++ BackEnd/FirstBackEnd/src/controllers/auth.controller.js
@@ -1,12 +1,49 @@
+import User from "../models/user.model.js";
+
+export const RegisterUser = async (req, res, next) => {
+  try {
+    const { fullName, email, phone, gender, password, dob } = req.body;
+
+    if (!fullName || !email || !password || !phone || !gender || !dob) {
+      const error = new Error("All Feilds Required");
+      error.statusCode = 400;
+      return next(error);
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      const error = new Error("Email Already Registered");
+      error.statusCode = 409;
+      return next(error);
+    }
+
+    const photoUrl = `https://placehold.co/600x400?text=${fullName.charAt(0).toUpperCase()}`;
+
+    const photo = {
+      url: photoUrl,
+      publicId: null,
+    };
+
+    const newUser = await User.create({
+      fullName,
+      email,
+      phone,
+      gender,
+      password,
+      dob,
+      photo,
+    });
+
+    res.status(201).json({ message: "User Created Succesfully" });
+  } catch (error) {
+    next();
+  }
+};
+
 export const LoginUser = (req, res) => {
   res.json({ message: "Login Sucessfull from Controller" });
 };
 
 export const LogoutUser = (req, res) => {
   res.json({ message: "Logout Sucessfull from Controller" });
-};
-
-
-export const RegisterUser = (req, res) => {
-  res.json({ message: "Registration Sucessfull from Controller" });
 };
